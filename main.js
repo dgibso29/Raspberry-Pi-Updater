00cb8a122787f8028f9dab9d8d5153617f3ec3fe
dgibso29 <gibson@earringpranks.com>
2020-01-24 10:35:01 -0500

    Initial work on connecting to FTP server
    Time to let the adults take charge

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+    response.end();
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
@@ -12,12 +12,66 @@ server.listen(3025);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:3025/");
+setInterval(CheckForNewVersion, 60000);
 
 
 // Checks the FTP server version file against the locally installed version.
 function CheckForNewVersion() {
 
+    var currVersion = GetFTPServerVersion();
+    var installedVersion = GetInstalledVersion();
 
+    if(currVersion == installedVersion)
+    {
+        return;
+    }
+    // If we got here, it's time to update.
 }
 
-setInterval(CheckForNewVersion, 60000);
+function GetFTPServerVersion()
+{
+    var currentVersion = "";
+
+    var Client = require('ftp');
+
+    var c = new Client();
+    c.on('ready', function() {
+        c.list(function(err, list) {
+            if (err) throw err;
+            console.dir(list);
+            c.end();
+        });
+    });
+    // connect to localhost:21 as anonymous
+    c.connect('10.146.0.110', 21, 'Fluid', 'Password1!');
+    c.get()
+    c.end();
+
+    return currentVersion;
+}
+
+function GetInstalledVersion()
+{
+    var installedVersion = "";
+
+    return installedVersion;
+}
+
+function GetUpdatedVersion()
+{
+    var Client = require('ftp');
+
+    var c = new Client();
+    c.on('ready', function() {
+        c.list(function(err, list) {
+            if (err) throw err;
+            console.dir(list);
+            c.end();
+        });
+    });
+    // connect to localhost:21 as anonymous
+    c.connect();
+
+    c.end();
+}
+
