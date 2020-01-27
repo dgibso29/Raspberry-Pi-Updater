48f1b55e07916f2bb808e01f2a18ae164be2d993
dgibso29 <gibson@earringpranks.com>
2020-01-27 12:52:59 -0500

    Got rid of pointless server

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ var ftp = require('basic-ftp');
 let config;
 
 // Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
+/*var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end();
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(3025);
+server.listen(3025);*/
 
 // Put a friendly message on the terminal
 //console.log("Server running at http://127.0.0.1:3025/");
