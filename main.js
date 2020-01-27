287182f7fc81278959d1d68aa707cd38716d453d
dgibso29 <gibson@earringpranks.com>
2020-01-27 11:45:49 -0500

    System now working as intended at base level
    Still need to implement handling of multiple applications

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // Load the http module to create an http server.
 var http = require('http');
+var fs = require('fs');
+var ftp = require('basic-ftp');
+
+let config;
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
@@ -11,98 +15,127 @@ var server = http.createServer(function (request, response) {
 server.listen(3025);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:3025/");
-setInterval(CheckForNewVersion, 60000);
-var test = GetFTPServerVersion();
+//console.log("Server running at http://127.0.0.1:3025/");
+setInterval(CheckForNewVersion, 5000);
 
-console.log("Got here! Value is " + test);
+function GetUpdaterConfig()
+{
+    fs.readFile('/home/pi/NAPS/UpdaterConfig.json', (err, data) => {
+        if (err) throw err;
+        //console.log(data.toString());
+        config = JSON.parse(data.toString());
+        //console.log(config);
+    })
+}
 
 // Checks the FTP server version file against the locally installed version.
 async function CheckForNewVersion() {
+    config = GetUpdaterConfig();
+    await new Promise(r => setTimeout(r, 500));
 
-    var currVersion = GetFTPServerVersion();
-    var installedVersion = GetInstalledVersion();
+    var currVersion = await GetFTPServerVersion();
+    var installedVersion = await GetInstalledVersion();
 
-
+    console.log('Latest version is ' + currVersion);
+    console.log('Installed version is ' + installedVersion);
 
     if (currVersion == installedVersion) {
         return;
-    }
+        }
     // If we got here, it's time to update.
+    await GetUpdatedVersion();
+
 }
 
 async function GetFTPServerVersion() {
-    var currentVersion = "default value";
+    var currentVersion = "";
 
     var Client = require('ftp');
 
     var c = new Client();
-    var config = {
-        host: "10.146.0.110",
-        user: "FCS-DEV02\\Fluid",
-        password: "Password1!"
+    var clientConfig = {
+        host: config.FtpHost,
+        port: config.FtpPort,
+        user: config.User,
+        password: config.Password
     };
 
-    c.connect(config);
-    console.log("huh");
-    c.get('LatestProbeVersion/ProbeVersion.json', false, function (err, responseStream) {
+    c.connect(clientConfig);
+    var path = "";
+    path = path.concat(config.RemoteDir, config.VersionFilePath);
+    c.get(path, false, function (err, responseStream) {
         if (err) throw err;
-        // console.log(err)
-        //responseStream.setEncoding('utf8');
-        //
-        //console.log(responseStream);
-        //
-        //
-        // console.log("ha ha loser");
 
-        console.log("Okay...")
         var content = '';
         responseStream.on('data', function (chunk)
         {
             content += chunk.toString();
-            console.log(content);
+            //console.log(content);
         });
         responseStream.on('end', function ()
         {
-            console.log(content);
+            //console.log(content);
 
              JSON.parse(content, function(key, value){
                  if(key == "Version")
-                     currentVersion == value;
+                     currentVersion = value;
              });
-
         });
-        console.log(content.toString());
+        //console.log(content.toString());
 
     })
     c.end();
+    await new Promise(r => setTimeout(r, 500));
 
-    console.log("Wait time?");
-    await new Promise(r => setTimeout(r, 2000));
-console.log("???SIFJF");
     return currentVersion;
 }
 
-function GetInstalledVersion() {
-    var installedVersion = "";
+async function GetInstalledVersion() {
+    var installedVersion = "Not Found";
+
+    console.log("Got here");
+
+    var path = "";
+    path = path.concat(config.LocalDir, config.VersionFilePath);
+
+    try{
+    fs.readFile(path, (err, data) => {
+        if (err) throw err;
+        //console.log(data.toString());
+        var versionJson = JSON.parse(data.toString(), function (key, value) {
+            if (key == "Version")
+                installedVersion = value;
+        });
+    });
+}
+catch{
+        // Weep.
+}
+
+    await new Promise(r => setTimeout(r, 500));
 
     return installedVersion;
 }
 
-function GetUpdatedVersion() {
-    var Client = require('ftp');
+async function GetUpdatedVersion() {
+    var client = new ftp.Client()
+    client.ftp.verbose = true
+    try {
+        await client.access({
+            host: config.FtpHost,
+            port: config.FtpPort,
+            user: config.User,
+            password: config.Password
+        })
 
-    var c = new Client();
-    c.on('ready', function () {
-        c.list(function (err, list) {
-            if (err) throw err;
-            console.dir(list);
-            c.end();
-        });
-    });
-    // connect to localhost:21 as anonymous
-    c.connect();
-
-    c.end();
+        console.log(await client.list())
+        await client.downloadToDir(config.LocalDir, config.RemoteDir)
+    }
+    catch(err) {
+        console.log(err)
+    }
+    client.close()
 }
+
+
 
