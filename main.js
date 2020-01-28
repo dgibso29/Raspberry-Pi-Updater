9c3161beaca2c0db25621b01abb2c03ae4bc240b
dgibso29 <gibson@earringpranks.com>
2020-01-28 12:21:43 -0500

    Disobyed Dave

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ server.listen(3025);*/
 
 // Put a friendly message on the terminal
 //console.log("Server running at http://127.0.0.1:3025/");
-setInterval(CheckForNewVersions, 5000);
+setInterval(CheckForNewVersions, 60000);
 
 function CreateFileStructure()
 {
